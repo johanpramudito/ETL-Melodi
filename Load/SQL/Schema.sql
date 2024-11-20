af94658477e7f99a4896c867a9b5e47aa2cab2e6
Grandiv <grandivfarand@gmail.com>
2024-11-20 22:45:57 +0700

    ERD and Schema

--- Load/SQL/Schema.sql
+++ Load/SQL/Schema.sql
@@ -1,72 +1,131 @@
-DROP TABLE IF EXISTS top_imdb;
-DROP TABLE IF EXISTS movie;
-DROP TABLE IF EXISTS movie_actor;
-DROP TABLE IF EXISTS movie_director;
-DROP TABLE IF EXISTS streaming_service;
-DROP TABLE IF EXISTS netflix_movie;
+BEGIN;
 
-create table top_imdb(
-        imdb_id  VARCHAR  not null,
-    movie_title  VARCHAR  not null,
-    PRIMARY KEY  (imdb_id)
+
+CREATE TABLE IF NOT EXISTS public.google_movie_streaming
+(
+    google_service_id character varying COLLATE pg_catalog."default" NOT NULL,
+    price character varying COLLATE pg_catalog."default" NOT NULL,
+    title character varying COLLATE pg_catalog."default",
+    imdb_id character varying COLLATE pg_catalog."default"
 );
 
-create table movie(
-        imdb_id  VARCHAR  not null,
-      imdb_rank  INT      not null,
-          title  VARCHAR  not null,
-           year  INT      not null,
-        runtime  DECIMAL  not null,
-          rated  VARCHAR  not null,
-    imdb_rating  DECIMAL  not null,
-    production   VARCHAR  not null,    
-    PRIMARY KEY  (imdb_id),
-    FOREIGN KEY  (imdb_id) REFERENCES top_imdb (imdb_id) 
+CREATE TABLE IF NOT EXISTS public.google_streaming_service
+(
+    service_id character varying COLLATE pg_catalog."default" NOT NULL,
+    service_name character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT google_streaming_service_pkey PRIMARY KEY (service_id)
 );
 
-create table movie_actor(
-        imdb_id  VARCHAR  not null,
-          actor  VARCHAR  not null,
-    FOREIGN KEY  (imdb_id) REFERENCES top_imdb (imdb_id) 
+CREATE TABLE IF NOT EXISTS public.movie
+(
+    imdb_id character varying COLLATE pg_catalog."default" NOT NULL,
+    imdb_rank integer NOT NULL,
+    title character varying COLLATE pg_catalog."default" NOT NULL,
+    year integer NOT NULL,
+    runtime numeric NOT NULL,
+    rated character varying COLLATE pg_catalog."default" NOT NULL,
+    imdb_rating numeric NOT NULL,
+    CONSTRAINT movie_pkey PRIMARY KEY (imdb_id),
+    CONSTRAINT title_unique UNIQUE (title)
 );
 
-
-create table movie_director(
-        imdb_id  VARCHAR  not null,
-       director  VARCHAR  not null,
-    FOREIGN KEY  (imdb_id) REFERENCES top_imdb (imdb_id) 
+CREATE TABLE IF NOT EXISTS public.movie_actor
+(
+    imdb_id character varying COLLATE pg_catalog."default" NOT NULL,
+    actor character varying COLLATE pg_catalog."default" NOT NULL
 );
 
-
-create table streaming_service(
-     service_id  VARCHAR  not null,
-   service_name  VARCHAR  not null,
-    PRIMARY KEY  (service_id) 
+CREATE TABLE IF NOT EXISTS public.movie_director
+(
+    imdb_id character varying COLLATE pg_catalog."default" NOT NULL,
+    director character varying COLLATE pg_catalog."default" NOT NULL
 );
 
-create table google_movie_streaming(
-            id  INT       not null,
-       imdb_id  VARCHAR   not null,
-    service_id  VARCHAR   not null,
-         price  VARCHAR   not null,
-   PRIMARY KEY  (id,imdb_id),    
-   FOREIGN KEY  (imdb_id)    REFERENCES top_imdb (imdb_id),
-   FOREIGN KEY  (service_id) REFERENCES streaming_service (service_id) 
+CREATE TABLE IF NOT EXISTS public.netflix_movie
+(
+    netflix_id integer NOT NULL,
+    imdb_id character varying COLLATE pg_catalog."default" NOT NULL,
+    title character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT netflix_movie_pkey PRIMARY KEY (netflix_id)
 );
 
-create table utelly_movie_streaming(
-            id  INT       not null,
-       imdb_id  VARCHAR   not null,
-    service_id  VARCHAR   not null,
-   PRIMARY KEY  (id,imdb_id),    
-   FOREIGN KEY  (imdb_id)    REFERENCES top_imdb (imdb_id),
-   FOREIGN KEY  (service_id) REFERENCES streaming_service (service_id) 
+CREATE TABLE IF NOT EXISTS public.top_imdb
+(
+    imdb_id character varying COLLATE pg_catalog."default" NOT NULL,
+    title character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT top_imdb_pkey PRIMARY KEY (imdb_id)
 );
 
-create table netflix_movie(
-             id  INT not null,
-        imdb_id  VARCHAR not null,
-          title  VARCHAR not null,
-    PRIMARY KEY  (id),
-    FOREIGN KEY  (imdb_id) REFERENCES top_imdb (imdb_id)
+CREATE TABLE IF NOT EXISTS public.utelly_movie_streaming
+(
+    imdb_id character varying COLLATE pg_catalog."default" NOT NULL,
+    service_id character varying COLLATE pg_catalog."default" NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS public.utelly_streaming_service
+(
+    service_id character varying COLLATE pg_catalog."default" NOT NULL,
+    service_name character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT streaming_service_pkey PRIMARY KEY (service_id)
+);
+
+ALTER TABLE IF EXISTS public.google_movie_streaming
+    ADD CONSTRAINT google_movie_streaming_imdb_id_fkey FOREIGN KEY (imdb_id)
+    REFERENCES public.top_imdb (imdb_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public.google_movie_streaming
+    ADD CONSTRAINT google_movie_streaming_service_id_fkey FOREIGN KEY (google_service_id)
+    REFERENCES public.google_streaming_service (service_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public.movie
+    ADD CONSTRAINT movie_imdb_id_fkey FOREIGN KEY (imdb_id)
+    REFERENCES public.top_imdb (imdb_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
+CREATE INDEX IF NOT EXISTS movie_pkey
+    ON public.movie(imdb_id);
+
+
+ALTER TABLE IF EXISTS public.movie_actor
+    ADD CONSTRAINT movie_actor_imdb_id_fkey FOREIGN KEY (imdb_id)
+    REFERENCES public.top_imdb (imdb_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
+
+
+ALTER TABLE IF EXISTS public.movie_director
+    ADD CONSTRAINT movie_director_imdb_id_fkey FOREIGN KEY (imdb_id)
+    REFERENCES public.top_imdb (imdb_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
+
+
+ALTER TABLE IF EXISTS public.netflix_movie
+    ADD CONSTRAINT netflix_movie_imdb_id_fkey FOREIGN KEY (imdb_id)
+    REFERENCES public.top_imdb (imdb_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
+
+
+ALTER TABLE IF EXISTS public.utelly_movie_streaming
+    ADD CONSTRAINT utelly_movie_streaming_imdb_id_fkey FOREIGN KEY (imdb_id)
+    REFERENCES public.top_imdb (imdb_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
+
+
+ALTER TABLE IF EXISTS public.utelly_movie_streaming
+    ADD CONSTRAINT utelly_movie_streaming_service_id_fkey FOREIGN KEY (service_id)
+    REFERENCES public.utelly_streaming_service (service_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
+
+END;
